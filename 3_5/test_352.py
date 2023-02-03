c78d15e3bf13f1b6487fb2e8b29e0fbb71a1226c
KorPavel <kor-p@bk.ru>
2023-02-03 14:09:54 +0300

    Create test_352.py

---
+++ 3_5/test_352.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        print('\n\t\tTEST#1 - "гость должен увидеть ссылку для входа"')
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print('\n\t\tTEST#2 - "гость должен увидеть ссылку на корзину на главной странице"')
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
